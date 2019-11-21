4ebc2dc7c957250c89cd231624e9db64576cc90d
Adela Gherga <adelagherga@gmail.com>
2019-11-20 23:18:11 -0800

    Remove Elpa direcory

--- elpa/magma-mode-20181205.1708/magma-mode-autoloads.el
+++
@@ -1,96 +0,0 @@
-;;; magma-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "magma-completion" "magma-completion.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from magma-completion.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-completion" '("magma-")))
-
-;;;***
-
-;;;### (autoloads nil "magma-extra" "magma-extra.el" (0 0 0 0))
-;;; Generated autoloads from magma-extra.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-extra" '("magma-")))
-
-;;;***
-
-;;;### (autoloads nil "magma-font-lock" "magma-font-lock.el" (0 0
-;;;;;;  0 0))
-;;; Generated autoloads from magma-font-lock.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-font-lock" '("magma-")))
-
-;;;***
-
-;;;### (autoloads nil "magma-interactive" "magma-interactive.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from magma-interactive.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-interactive" '("magma-")))
-
-;;;***
-
-;;;### (autoloads nil "magma-mode" "magma-mode.el" (0 0 0 0))
-;;; Generated autoloads from magma-mode.el
-
-(autoload 'magma-mode "magma-mode" "\
-Magma mode
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "magma-q" "magma-q.el" (0 0 0 0))
-;;; Generated autoloads from magma-q.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-q" '("magma-q-")))
-
-;;;***
-
-;;;### (autoloads nil "magma-scan" "magma-scan.el" (0 0 0 0))
-;;; Generated autoloads from magma-scan.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-scan" '("magma-")))
-
-;;;***
-
-;;;### (autoloads nil "magma-smie" "magma-smie.el" (0 0 0 0))
-;;; Generated autoloads from magma-smie.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-smie" '("magma-")))
-
-;;;***
-
-;;;### (autoloads nil "magma-snippets" "magma-snippets.el" (0 0 0
-;;;;;;  0))
-;;; Generated autoloads from magma-snippets.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-snippets" '("magma-s")))
-
-;;;***
-
-;;;### (autoloads nil "magma-vars" "magma-vars.el" (0 0 0 0))
-;;; Generated autoloads from magma-vars.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magma-vars" '("magma-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("magma-mode-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; magma-mode-autoloads.el ends here
